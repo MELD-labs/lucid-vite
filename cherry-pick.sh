c9ec40d71cfd9d20407d108c3f2a660f308f8187
Dat Duong <DuongDat.Informatics@rocketmail.com>
2024-09-05 13:39:07 +0700

    Add cherry-pick script

---
+++ cherry-pick.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git reset --hard fork/fork-base
+git cherry-pick fork/fork-base..fork/build
+git cherry-pick fork/fork-base..fork/blueprint
+git cherry-pick fork/fork-base..fork/wallet-from-seed-utxos
+git cherry-pick fork/fork-base..fork/native-script
+deno task pack
+git add .
+git commit -m "Rebuild artifact"
