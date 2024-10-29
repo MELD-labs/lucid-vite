b0bdd4e0665c2fbb473823c19f18e86b94e4a320
cristiano <cristiano.santos.martinss@gmail.com>
2024-10-29 10:05:00 +0100

    Rebuild artifact

--- src/core/core.ts
+++ src/core/core.ts
@@ -1,30 +1,14 @@
-import * as C from "./libs/cardano_multiplatform_lib/cardano_multiplatform_lib.generated.js";
-import * as M from "./libs/cardano_message_signing/cardano_message_signing.generated.js";
-import packageJson from "../../package.json" with { type: "json" };
+import DCSParkTypes from "../types/cardano_multiplatform_lib.generated";
+import EmurgoTypes from "../types/cardano_message_signing.generated";
 
-async function unsafeInstantiate(module: any, url: string) {
-  try {
-    await module.instantiate({
-      // Exception for Deno fresh framework
-      url: new URL(
-        url,
-        `https://deno.land/x/lucid@${packageJson.version}/src/core/libs/`,
-      ),
-    });
-  } catch (_e) {
-    // This only ever happens during SSR rendering
+declare global {
+  interface Window {
+    DCSpark: typeof DCSParkTypes;
+    Emurgo: typeof EmurgoTypes;
   }
 }
 
-await Promise.all([
-  unsafeInstantiate(
-    C,
-    `cardano_multiplatform_lib/cardano_multiplatform_lib_bg.wasm`,
-  ),
-  unsafeInstantiate(
-    M,
-    `cardano_message_signing/cardano_message_signing_bg.wasm`,
-  ),
-]);
+const C = window.DCSpark;
+const M = window.Emurgo;
 
 export { C, M };
